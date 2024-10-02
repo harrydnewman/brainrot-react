ed782c57def61ec739165bee19380771efe7500d
Harry Newman <harrisonnewman@Harrys-MacBook-Pro.local>
2024-10-02 04:09:49 +0200

    rebasing

---
+++ src/pages/Animation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import Navbar from '../components/Navbar'
+
+const Animation = () => {
+  return (
+    <div>
+        {/* <Navbar /> */}
+        <h1>Animation</h1>
+    </div>
+  )
+}
+
+export default Animation
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,5 +10,5 @@ const Contact = () => {
     </div>
   )
 }
-// hi
+
 export default Contact
