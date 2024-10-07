2d6e105de0726e9f23e95e50a0081bdad9528780
Harry Newman <harrisonnewman@Harrys-MacBook-Pro.local>
2024-10-08 01:01:14 +0200

    styling for scroll page

--- src/components/ScrollSource.js
+++ src/components/ScrollSource.js
@@ -3,7 +3,7 @@ import styles from '../styles/ScrollSource.module.css'
 export default function ScrollSource() {
     return (
         <div className={styles.scrollSourceContainer}>
-            <h1>Video source Gonna Go Here Mama</h1>
+            <p>Video source Gonna Go Here Mama</p>
         </div>
     )
 }
--- src/pages/Scroll.js
+++ src/pages/Scroll.js
@@ -1,18 +1,73 @@
-import React from 'react';
-import styles from '../styles/Scroll.module.css'
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/Scroll.module.css';
 import LeftScroll from '../components/LeftScroll';
 import RightScroll from '../components/RightScroll';
 import ScrollSource from '../components/ScrollSource';
 
 const Scroll = () => {
-  
-  return (
-    <div className={styles.scrollContainer}>
-      <LeftScroll/>
-      <RightScroll/>
-      <ScrollSource />
-    </div>
-  );
-}
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    // Function to update state with the new window size
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+      console.log(`Width: ${window.innerWidth}, Height: ${window.innerHeight}`);
+    };
+
+    // Add event listener to resize event
+    window.addEventListener('resize', handleResize);
+
+    // Log initial screen size
+    console.log(`Initial Width: ${window.innerWidth}, Initial Height: ${window.innerHeight}`);
+
+    // Cleanup function to remove event listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  // Use the tracked windowSize for conditional rendering
+  if (windowSize.width < 500) {
+    return (
+      <div className={styles.mobileScrollContainer}>
+        <div className={styles.mobileLeftScrollContainer}>
+          <LeftScroll />
+        </div>
+        <div className={styles.mobileRightScrollContainer}>
+          <RightScroll />
+        </div>
+        <div className={styles.mobileSourceScrollContainer}>
+          <ScrollSource />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.flexScrollContainer}>
+       <div className={styles.leftFlex}>
+       <div className={styles.leftScrollContainer}>
+          <LeftScroll />
+        </div>
+        <div className={styles.sourceScrollContainer}>
+          <ScrollSource />
+        </div>
+       </div>
+       <div className={styles.rightFlex}>
+       <div className={styles.rightScrollContainer}>
+          <RightScroll />
+        </div>
+       
+       </div>
+        
+      </div>
+    );
+  }
+};
 
 export default Scroll;
