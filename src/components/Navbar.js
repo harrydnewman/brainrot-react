2332478af799fdfb902a21d849cb89e6016cceba
Harry Newman <harrisonnewman@Harrys-MacBook-Pro.local>
2024-10-08 06:37:19 +0200

    finna start animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,40 @@ import { Link } from "react-router-dom";
 import styles from '../styles/Navbar.module.css'
 
 const Navbar = () => {
+
+  function selectScroll(){
+    console.log("scroll selected!")
+  }
+
+  function selectGrid(){
+    console.log("Grid selected");
+  }
   return (
     <div className={styles.navbarDiv}>
       <div className={styles.scrollTypeButtons}>
+        <div className={styles.typeButtonDrawer}>
+        <div className={`${styles.drawerSection} ${styles.rightBorder}`}>
+          <button className={styles.drawerButton} onClick={selectScroll}></button>
+        </div>
+        <div className={`${styles.drawerSection} ${styles.leftBorder}`}>
+        <button className={styles.drawerButton} onClick={selectGrid}></button>
+        </div>
+        </div>
+      </div>
+      {/* <div className={styles.scrollTypeButtons}>
         <div className={styles.typeButton}>
 
         </div>
         <div className={styles.typeButton}>
           
         </div>
-      </div>
-      <div className={styles.aboutButtonLink}>
+      </div> */}
+      <div className={styles.aboutLinkHolder}>
+        <div className={styles.aboutButtonLink}>
 
+        </div>
       </div>
+
     </div>
   );
 }
