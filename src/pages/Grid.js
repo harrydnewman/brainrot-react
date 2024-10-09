2177f6ac61add9cd87b274ca849339b889c14dce
Harry Newman <harrisonnewman@Harrys-MacBook-Pro.local>
2024-10-09 11:44:53 +0200

    starting grid

--- src/pages/Grid.js
+++ src/pages/Grid.js
@@ -1,8 +1,8 @@
-
+import styles from '../styles/Grid.module.css'
 
 export default function Grid(){
     return (
-        <div>
+        <div className={styles.mainDiv}>
             <p>GRID</p>
         </div>
     )
